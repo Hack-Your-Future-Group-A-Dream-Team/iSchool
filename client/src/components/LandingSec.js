2bbf4edb32a9616a8d043d0467be505a23218502
fatih <fatihkarakus@hotmail.com>
2021-01-18 13:08:30 +0100

    Landing page design fix

--- client/src/components/LandingSec.js
+++ client/src/components/LandingSec.js
@@ -8,8 +8,8 @@ import { Link } from 'react-router-dom';
 function LandingSec() {
     return ( 
         <div className = 'sec-container' >
-        <p> Finding the right school for your child just got easier <br/> 
-            iSchool helps you find the best school for your child by offering the most proper schools according to your preferences and your address.
+        <p> Finding the right school for your child just got easier! <br/> 
+            iSchool helps you find the best school for your child by a few clicks.
         </p> 
         <div className = 'sec-btns' >
             <Button className = 'btns'
@@ -19,9 +19,9 @@ function LandingSec() {
             </Button>
         </div> 
 
-        <div>
-             <p className="text"> By signing-up you can save your favorite schools in your account, as well as give ratings to schools and comment about them.  <br/>
-             If You Are School Manager And You Want To Register Your School, Please <Link to='/register' style={{color: '#B71C1C', fontWeight: 'bold', textDecoration: "none"}}>SIGN UP</Link>! </p> 
+        <div className='signup'>
+             <p className="text"> By <Link to='/register' style={{color: '#B71C1C', fontWeight: 'bold', textDecoration: "none"}}>signing-up</Link> you can save your favorite schools in your account, as well as give ratings to schools and comment about them.  <br/>
+             If you are a school manager and you want to register your school, please <Link to='/register' style={{color: '#B71C1C', fontWeight: 'bold', textDecoration: "none"}}>SIGN UP</Link>! </p> 
        </div>
         <div className="student">
             <img src={Background} alt="student"/>
